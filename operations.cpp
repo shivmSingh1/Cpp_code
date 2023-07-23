8e96e9f1cb6fb8594347fe657e6d0d9fd5e96e8c
shivmSingh1 <shivams7380@gmai.com>
2023-07-24 04:53:31 +0530

    different operatios on variables

---
+++ operations.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int a,b;
+
+    cout<<"Enter first number ";
+    cin>>a;
+    cout<<"Enter second number ";
+    cin>>b;
+
+    cout<<"Value of a - b is equal to "<< a - b<<endl;
+    cout<<"Value of a + b is equal to "<< a + b<<endl;
+    cout<<"Value of a * b is equal to "<< a * b<<endl;
+    cout<<"Value of a / b is equal to "<< a / b<<endl;
+
+    return 0;
+
+}
