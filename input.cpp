c7e8ec290af8400b19e9025f8e83cf100d950862
shivmSingh1 <shivams7380@gmai.com>
2023-07-24 04:30:22 +0530

    take input from user and print output

---
+++ input.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int a, b;
+    cout<<"Enter first number ";
+    cin>>a;
+    cout<<"Enter second number ";
+    cin>>b;
+
+    cout<<"Sum of your number is "<<a+b;
+
+    return 0;
+}
